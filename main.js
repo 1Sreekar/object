e4fae9944db6a7600017c73df550bc2c559227e3
1Sreekar <107261181+1Sreekar@users.noreply.github.com>
2023-01-19 15:53:37 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Tv(){
+    window.location="Tv.html";
+
+}
+
+function Chair(){
+    window.location="chair.html";
+
+}
+
+function Sofa(){
+    window.location="sofa.html";
+
+}
+
+function Table(){
+    window.location="table.html";
+
+}
